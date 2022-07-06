b5c0fc3f7a2c4236eda07afc7e7e054854bb2242
Nischal Shakya <nischalshakya2015@gmail.com>
2022-07-06 17:22:55 +0545

    feat: initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+Simple resume maker
---
+++ src/app/app.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+  return (
+    <main>
+      <p>This is the resume maker</p>
+    </main>
+  );
+};
+
+export default App;
