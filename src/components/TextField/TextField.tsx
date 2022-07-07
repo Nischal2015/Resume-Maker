4a6873d7626d0b6468a986169f979cf0f8a63fb0
Nischal Shakya <nischalshakya2015@gmail.com>
2022-07-08 00:10:38 +0545

    feat: add resume section

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,8 +3,10 @@ import styles from "./TextField.module.css";
 const TextField = ({ label }: { label: string }) => {
   return (
     <div className={styles.textfield}>
-      <label htmlFor="input">{label}</label>
-      <input type="text" id="input" className={styles.input} />
+      <label htmlFor='input' className={styles.label}>
+        {label}
+      </label>
+      <input type='text' id='input' className={styles.input} />
     </div>
   );
 };
